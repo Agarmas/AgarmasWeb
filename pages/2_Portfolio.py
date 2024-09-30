810cab58936b64fdeb938638ec0b6ab35dcf16dd
Antonio Garrido Masse <agarmas03@gmail.com>
2024-09-30 17:11:14 +0200

    Portfolio page

---
+++ pages/2_Portfolio.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Portfolio",
+    page_icon=":bar_chart:",
+)
+
+st.title("Portfolio")
+
+st.write("""
+### Projects :briefcase:
+1. **Odoo Coffee Management (with contributors)**  
+   Developed a custom Odoo module to manage coffee consumption and track stock levels for a coffee management system. Worked with a team of contributors to design and implement the solution.  
+   [View Repository](https://github.com/Agarmas/upocafe)
+""")
