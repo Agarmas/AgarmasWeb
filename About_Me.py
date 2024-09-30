5aacfcf491b857ce9fdf6e940ba209390201efe2
Antonio Garrido Masse <agarmas03@gmail.com>
2024-09-30 17:10:18 +0200

    About me page

---
+++ About_Me.py
@@ -0,0 +1,38 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="About me",
+    page_icon=":technologist:",
+)
+
+st.title("Antonio Garrido Massé")
+
+st.write("""
+I am a highly motivated **fourth-year Computer Engineering student** at Universidad Pablo de Olavide, with a deep passion for **Data Science**, **Machine Learning**, and **Open-Source Software**. Throughout my academic journey, I have gained extensive experience in **data analysis**, **algorithm development**, and **software engineering**.
+""")
+
+st.header("Education :mortar_board:")
+st.write("""
+- **Computer Engineering** *(2021 - 2025)*  
+  Universidad Pablo de Olavide, Seville, Spain  
+  Specialization in **Information Systems**.
+- **Online Certifications** in various technologies such as **[Codecademy](https://www.codecademy.com/profiles/Agarmas)** and **Programming Hub**.
+""")
+
+st.header("Professional Experience :briefcase:")
+st.write("""
+- **Research Intern - Big Data & Data Science Lab** *(03/2024 - 06/2024)*  
+  Universidad Pablo de Olavide  
+  Working on various data analysis projects, focusing on large datasets and developing machine learning models for academic research.
+
+- **Private Tutor (Mathematics, Physics, Programming)** *(2019 - 2022)*  
+  Provided personalized lessons in mathematics, physics, and programming (C and Java) to high school and university students.
+
+- **Treasurer - ESOLIUPO Association** *(2024 - Present)*  
+  Treasurer of the Open Source Software Association at Universidad Pablo de Olavide. Managed finances and organized events in collaboration with companies and institutions.
+""")
+
+st.header("Hobbies & Interests :bicyclist:")
+st.write("""
+In addition to my academic and professional life, I enjoy staying updated with the latest trends in **technology** and **data science**. I am also passionate about **open-source software** and make my own projects.
+""")
