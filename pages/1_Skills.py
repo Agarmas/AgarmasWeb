a4544404aec1662636423f197efb97405a289631
Antonio Garrido Masse <agarmas03@gmail.com>
2024-09-30 17:10:49 +0200

    Skills page

---
+++ pages/1_Skills.py
@@ -0,0 +1,55 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Skills",
+    page_icon=":muscle:",
+)
+
+st.title("Skills")
+
+st.write("""
+### Programming Languages :keyboard:
+- Python :snake:
+- Java :coffee: 
+- C :desktop_computer:
+- TypeScript :memo:
+- PHP :elephant:
+
+### Data Science Tools :bar_chart:
+- NumPy :abacus:
+- Pandas :panda_face:
+- Scikit-learn :chart_with_upwards_trend:
+- Streamlit :balloon:
+- KNIME :gear:
+- Orange :tangerine:
+
+### Web Development :globe_with_meridians:
+- Django :spider_web:
+- Express.js :package:
+- Laravel :rocket:
+- Java Spring :leafy_green:
+
+### Software Architecture :building_construction:
+- Software design patterns :triangular_ruler:
+- Database modeling :file_cabinet:
+- Project planning :calendar:
+
+### Database Management :floppy_disk:
+- SQL :ledger:
+- MongoDB :leafy_green:
+
+### ERP Development :briefcase:
+- Odoo modules development :factory:
+
+### Soft Skills :sparkles:
+- Teamwork :busts_in_silhouette:
+- Time management :clock1:
+- Problem-solving :thinking_face:
+- Proactivity :runner:
+- Quick learning :bulb:
+
+### Languages :speech_balloon:
+- Spanish (Native)
+- English (B2)
+- French (Basic)
+""")
