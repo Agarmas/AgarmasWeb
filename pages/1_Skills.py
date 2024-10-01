a64649b01e2de3219bb79716f2f6686b59d754dc
Antonio Garrido Masse <agarmas03@gmail.com>
2024-10-01 12:14:40 +0200

    Remove skills emojis

--- pages/1_Skills.py
+++ pages/1_Skills.py
@@ -9,44 +9,44 @@ st.title("Skills")
 
 st.write("""
 ### Programming Languages :keyboard:
-- Python :snake:
-- Java :coffee: 
-- C :desktop_computer:
-- TypeScript :memo:
-- PHP :elephant:
+- Python
+- Java
+- C
+- TypeScript
+- PHP
 
 ### Data Science Tools :bar_chart:
-- NumPy :abacus:
-- Pandas :panda_face:
-- Scikit-learn :chart_with_upwards_trend:
-- Streamlit :balloon:
-- KNIME :gear:
-- Orange :tangerine:
+- NumPy
+- Pandas
+- Scikit-learn
+- Streamlit
+- KNIME
+- Orange
 
 ### Web Development :globe_with_meridians:
-- Django :spider_web:
-- Express.js :package:
-- Laravel :rocket:
-- Java Spring :leafy_green:
+- Django
+- Express.js
+- Laravel
+- Java Spring
 
 ### Software Architecture :building_construction:
-- Software design patterns :triangular_ruler:
-- Database modeling :file_cabinet:
-- Project planning :calendar:
+- Software design patterns
+- Database modeling
+- Project planning
 
 ### Database Management :floppy_disk:
-- SQL :ledger:
-- MongoDB :leafy_green:
+- SQL
+- MongoDB
 
 ### ERP Development :briefcase:
-- Odoo modules development :factory:
+- Odoo modules development
 
 ### Soft Skills :sparkles:
-- Teamwork :busts_in_silhouette:
-- Time management :clock1:
-- Problem-solving :thinking_face:
-- Proactivity :runner:
-- Quick learning :bulb:
+- Teamwork
+- Time management
+- Problem-solving
+- Proactivity
+- Quick learning
 
 ### Languages :speech_balloon:
 - Spanish (Native)
