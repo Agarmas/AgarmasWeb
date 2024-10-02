18e4a4142ea5378231cabf81209e9284c967c8b9
Antonio Garrido Masse <agarmas03@gmail.com>
2024-10-02 18:14:05 +0200

    New project

--- pages/2_Portfolio.py
+++ pages/2_Portfolio.py
@@ -10,6 +10,10 @@ st.title("Portfolio")
 st.write("""
 ### Projects :briefcase:
 1. **Odoo Coffee Management (with contributors)**  
-   Developed a custom Odoo module to manage coffee consumption and track stock levels for a coffee management system. Worked with a team of contributors to design and implement the solution.  
-   [View Repository](https://github.com/Agarmas/upocafe)
+    Developed a custom Odoo module to manage coffee consumption and track stock levels for a coffee management system. Worked with a team of contributors to design and implement the solution.  
+    [View Repository](https://github.com/Agarmas/upocafe)
+2. **Data Visualization in remote work productivity analysis**
+    Use matplotlib & seaborn for data visualization and generate knowedge and data correlation.
+    [Web page](https://agarmas-remote-work-analysis.streamlit.app/)
+    [View repository](https://github.com/Agarmas/remote-work-analysis)
 """)
