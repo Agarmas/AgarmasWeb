c235bcebaf803734d14902919d46f0a555662317
Antonio Garrido Masse <agarmas03@gmail.com>
2024-10-02 18:15:59 +0200

    Visual

--- pages/2_Portfolio.py
+++ pages/2_Portfolio.py
@@ -13,7 +13,7 @@ st.write("""
     Developed a custom Odoo module to manage coffee consumption and track stock levels for a coffee management system. Worked with a team of contributors to design and implement the solution.  
     [View Repository](https://github.com/Agarmas/upocafe)
 2. **Data Visualization in remote work productivity analysis**\n
-    Use matplotlib & seaborn for data visualization and generate knowedge and data correlation.
-    [Web page](https://agarmas-remote-work-analysis.streamlit.app/)
+    Use matplotlib & seaborn for data visualization and generate knowedge and data correlation.\n
+    [Web page](https://agarmas-remote-work-analysis.streamlit.app/)\n
     [View repository](https://github.com/Agarmas/remote-work-analysis)
 """)
